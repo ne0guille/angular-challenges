de790525f9a1d7cddf464da45003a5c9aba8e7bb
Guillermo Eyherabide <eyherabideg@gmail.com>
2023-09-26 13:31:47 -0300

    feat(challenge): solve 1st challenge

--- apps/projection/src/app/ui/card/card.component.ts
+++ apps/projection/src/app/ui/card/card.component.ts
@@ -1,34 +1,34 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
-import { ListItemComponent } from '../list-item/list-item.component';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import {
+  Component,
+  ContentChild,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, ListItemComponent],
+  host: {
+    class: 'border-2 border-black rounded-md p-4 w-fit flex flex-col gap-3',
+  },
+  imports: [NgIf, NgFor, NgTemplateOutlet],
 })
 export class CardComponent {
   @Input() list: any[] | null = null;
-  @Input() type!: CardType;
-  @Input() customClass = '';
-
-  CardType = CardType;
-
-  constructor(
-    private teacherStore: TeacherStore,
-    private studentStore: StudentStore
-  ) {}
+  @Output() addItem = new EventEmitter<void>();
+  @Output() deleteItem = new EventEmitter<number>();
+  @ContentChild('row', { read: TemplateRef })
+  rowTemplate: TemplateRef<any> | null = null;
 
   addNewItem() {
-    if (this.type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (this.type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
+    this.addItem.emit();
+  }
+
+  onDelete(id: number) {
+    this.deleteItem.emit(id);
   }
 }
